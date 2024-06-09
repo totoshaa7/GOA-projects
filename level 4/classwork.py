e45fc418ab1ddc71607ec692962ed55e83f8729c
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-06-08 17:21:25 -0700

    goa

---
+++ level 4/classwork.py
@@ -0,0 +1,15 @@
+ # (1) (2) (3)
+name = 'otuli'
+last_name = 'meskhishvili'
+proffesion = 'web developer'
+
+print(name + ' ' + last_name + ' '+ proffesion)
+
+name = 'otuli'
+last_name = 'meskhishvili'
+hobby = 'football'
+country = 'georgia'
+
+
+print(name + ' + ' + last_name + ' + '+ hobby + ' + ' + country)
+
---
+++ level 4/homework.py
@@ -0,0 +1,19 @@
+name = 'otuli'
+last_name = 'meskhishvili'
+hobby = 'football'
+country = 'georgia'
+
+
+print(name + ' + ' + last_name + ' + '+ hobby + ' + ' + country) 
+
+
+
+name = 'otuli'
+last_name = 'meskhishvili'
+hobby = 'football'
+country = 'georgia'
+
+
+print(name + ' ' + last_name + ' '+ hobby + ' ' + country)
+
+print('football')
