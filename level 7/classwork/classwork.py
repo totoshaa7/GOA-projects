13b28b73654ebfd0e643753b3e95c81912401c8b
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-07-05 13:48:29 -0700

    goa

---
+++ level 7/classwork/classwork.py
@@ -0,0 +1,28 @@
+name = "123Anna"
+score = 96
+
+print(name)
+print(score)
+
+
+
+
+
+
+flight_n = "BAO117"
+destination = "new york"
+distance = 1500
+
+print("Flight Number:", flight_n)
+print("Destination:", destination)
+print("Distance:", distance)
+
+
+
+salary = 150000
+pay_raise = 10000
+
+# create a variable named new_salary
+
+new_salary = salary + pay_raise
+print(new_salary)
