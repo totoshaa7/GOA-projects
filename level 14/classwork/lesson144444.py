d69498fc05e79735139960a83bd98b0684f289ee
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-08-16 16:22:25 -0700

    goa

--- level 14/classwork/lesson144444.py
+++ level 14/classwork/lesson144444.py
@@ -2,7 +2,6 @@ age = int(input("enter your age:"))
 
 age==18<= age <=60
 
-else print("You are not eligible to participate because you must be either employed or a student.")
-else print("You are not eligible to participate because you must be at least 18 years old and not more than 60 years old.")
+
 
 
