747cc7a06853425e3b3ae96a7759336ee91515da
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-10-13 23:39:59 -0700

    goa

---
+++ level 11/home/lesson11.py
@@ -0,0 +1,15 @@
+
+number1 = int(input("enter first number: "))
+number2 = int(input("enter second number: "))
+
+
+
+print(number1 == number2)
+
+
+
+
+age = int(input("enter your age: "))
+
+
+print(age > 18)
