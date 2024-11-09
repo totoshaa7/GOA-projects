dc73d507401e0556d20448a83e862def81042e01
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-11-10 00:03:44 +0400

    Create lesson299.py

---
+++ level 28/class/lesson299.py
@@ -0,0 +1,28 @@
+def search(text, word):
+    if word in text:
+        return "სიტყვა ნაპოვნია"
+    else:
+        return "სიტყვა ნაპოვნია"
+
+print(search("ეს გასაოცარია", "გასაოცარია")) 
+
+#2
+
+def count_words(text):
+    words = text.split()  
+    return len(words)     
+
+text = "ეს არის ტექსტური წინადადება"
+print(count_words(text))  
+
+
+#3
+
+
+def check_number(number):
+    if number > 0:
+        return "რიცხვი დადებითია"
+    elif number < 0:
+        return "რიცხვი უარყოფითია"
+    else:
+        return "რიცხვი ნულია"
