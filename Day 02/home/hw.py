3c7aa397906341426bb860acbd43b99697f9029e
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-12-09 05:21:48 +0400

    goa

---
+++ Day 02/home/hw.py
@@ -0,0 +1,7 @@
+#1) გაიცანით თქვენი ჯგუფელები, დაიმეგობრეთ, და გაუზიარეთ შთაბეჭდილებები, იმეცადინეთ ერთად, შეიყვარეთ ერთმანეთი. 
+#ვისაც გაიცნობთ, ყველას სახელი და გვარი ამოიწერეთ, level2.txt ფაილში და ატვირთეთ github _ ზე
+
+print("akaki dolidze")
+print("nikoloz kobakhidze")
+print("luka makharadze")
+print("nikoloz cukoshvili")
