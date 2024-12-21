42f283058c9124635351322f5d9d3c57f4f83313
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2024-12-21 11:15:58 +0400

    goa

---
+++ level 21/classwork/lesson21.py
@@ -0,0 +1,21 @@
+
+fruits = ["apple", "banana"]
+fruits.append("orange") 
+print(fruits)
+
+
+fruits.insert(1, "grape") 
+print(fruits)  
+
+
+lastname = input("შეიყვანეთ თქვენი გვარი: ")
+print(f"თქვენი გვარის სიგრძე არის: {len(lastname)}")
+
+movies = ["Avatar", "Titanic", "Avengers", "GOA", "Group 30"]
+movies.insert(3, "თამარი") 
+print(movies)  
+
+cars = ["BMW", "Audi", "Honda"]
+cars.append("Toyota")  
+print(cars) 
+
