7d0865b9116061ae015874ef378b0d4b16094c7b
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2025-03-10 11:09:20 +0400

    goa

---
+++ level 47/class/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt("enter your name: ");
+let lastName = prompt("enter your last name: ");
+let age = prompt("enter your age: ");
+let city = prompt("enter your street: ");
+
+let sentence = `${firstName} ${lastName} is ${age} years old and lives on ${city} street.`;
+
+alert(sentence)
