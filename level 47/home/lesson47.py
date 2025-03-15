8259b90bf022215c8281975c5db0ddb11b3b263d
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2025-03-15 17:02:41 +0400

    goa

---
+++ level 47/home/lesson47.py
@@ -0,0 +1,20 @@
+
+def main():
+    
+    first_name = input("შეიყვანეთ თქვენი სახელი: ")
+    last_name = input("შეიყვანეთ თქვენი გვარი: ")
+    age = input("შეიყვანეთ თქვენი ასაკი: ")
+    location = input("შეიყვანეთ საცხოვრებელი ადგილი: ")
+    mother_first_name = input("შეიყვანეთ დედის სახელი: ")
+    mother_last_name = input("შეიყვანეთ დედის გვარი: ")
+    mother_age = input("შეიყვანეთ დედის ასაკი: ")
+    
+  
+    sentence = (f"მე ვარ {first_name} {last_name}, {age} წლის ვცხოვრობ {location}-ში "
+                f"ჩემი დედაა {mother_first_name} {mother_last_name} რომელიც არის {mother_age} წლის")
+    
+  
+    print("\n" + sentence)
+
+if __name__ == "__main__":
+    main()
