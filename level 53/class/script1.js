283c1b38333237e9a1a889bdeb0cb845e30c0eec
totoshaa7 <168858482+totoshaa7@users.noreply.github.com>
2025-05-03 16:52:59 +0400

    goa

---
+++ level 53/class/script1.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 200; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+}
+for (let i = 0; i < 3; i++) {
+    console.log("GOA IS THE BEST");
+}  
+
+SSSSSSSSSSSSSSSSSSSSSS
